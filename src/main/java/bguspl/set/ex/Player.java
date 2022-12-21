c0cb15e767c020f6fdc35d53c9593627b7bf0b05
EldorMouyal <93645504+EldorMouyal@users.noreply.github.com>
2022-12-21 10:21:20 +0200

    first commit

---
+++ src/main/java/bguspl/set/ex/Player.java
@@ -0,0 +1,186 @@
+package bguspl.set.ex;
+
+import bguspl.set.Env;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
+/**
+ * This class manages the players' threads and data
+ *
+ * @inv id >= 0
+ * @inv score >= 0
+ */
+public class Player implements Runnable {
+
+    /**
+     * The game environment object.
+     */
+    private final Env env;
+
+    /**
+     * Game entities.
+     */
+    private final Table table;
+
+    /**
+     * The id of the player (starting from 0).
+     */
+    public final int id;
+
+    /**
+     * The thread representing the current player.
+     */
+    private Thread playerThread;
+
+    /**
+     * The thread of the AI (computer) player (an additional thread used to generate key presses).
+     */
+    private Thread aiThread;
+
+    /**
+     * True iff the player is human (not a computer player).
+     */
+    private final boolean human;
+    private final Dealer dealer;
+
+    /**
+     * True iff game should be terminated.
+     */
+    private volatile boolean terminate;
+
+    /**
+     * The current score of the player.
+     */
+    private int score;
+    private int tokensPlaced;
+
+    private Queue<Integer> actionsTodo;
+    /**
+     * The class constructor.
+     *
+     * @param env    - the environment object.
+     * @param dealer - the dealer object.
+     * @param table  - the table object.
+     * @param id     - the id of the player.
+     * @param human  - true iff the player is a human player (i.e. input is provided manually, via the keyboard).
+     */
+    public Player(Env env, Dealer dealer, Table table, int id, boolean human) {
+        this.env = env;
+        this.table = table;
+        this.id = id;
+        this.human = human;
+        this.dealer=dealer;
+        this.tokensPlaced = 0;
+        actionsTodo= new LinkedList<>();
+    }
+
+    /**
+     * The main player thread of each player starts here (main loop for the player thread).
+     */
+    @Override
+    public void run() {
+        playerThread = Thread.currentThread();
+        env.logger.info("Thread " + Thread.currentThread().getName() + " starting.");
+        if (!human) createArtificialIntelligence();
+
+        while (!terminate) {
+            // TODO implement main player loop
+            try {
+                    if (!actionsTodo.isEmpty()) {
+                        Integer slot = actionsTodo.remove();
+                        if (!table.removeToken(this.id, slot)) {
+                            env.ui.placeToken(this.id, slot);
+                            table.placeToken(this.id, slot);
+                            tokensPlaced++;
+                            if (tokensPlaced == 3) {
+                                if (this.dealer.CheckPlayerSet(this.id)) {
+                                    point();
+                                } else
+                                    penalty();
+                            }
+                        } else {
+                            tokensPlaced--;
+                        }
+                        //notifyAll();
+                        //wait();
+                    }
+            }
+            //catch (InterruptedException ignored) {}
+            catch (NoSuchElementException e) {
+                try {
+                    wait();
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        }
+        if (!human) try { aiThread.join(); } catch (InterruptedException ignored) {}
+        env.logger.info("Thread " + Thread.currentThread().getName() + " terminated.");
+    }
+
+    /**
+     * Creates an additional thread for an AI (computer) player. The main loop of this thread repeatedly generates
+     * key presses. If the queue of key presses is full, the thread waits until it is not full.
+     */
+    private void createArtificialIntelligence() {
+
+        // note: this is a very, very smart AI (!)
+        aiThread = new Thread(() -> {
+            env.logger.info("Thread " + Thread.currentThread().getName() + " starting.");
+            while (!terminate) {
+
+                // TODO implement player key press simulator
+                try {
+                        wait();
+                }
+                catch (InterruptedException ignored) {}
+
+            }
+            env.logger.info("Thread " + Thread.currentThread().getName() + " terminated.");
+        }, "computer-" + id);
+        aiThread.start();
+    }
+
+    /**
+     * Called when the game should be terminated.
+     */
+    public void terminate() {
+        // TODO implement
+    }
+
+    /**
+     * This method is called when a key is pressed.
+     *
+     * @param slot - the slot corresponding to the key pressed.
+     */
+    public void keyPressed(int slot) {
+        // TODO implement
+        actionsTodo.add(slot);
+    }
+
+    /**
+     * Award a point to a player and perform other related actions.
+     *
+     * @post - the player's score is increased by 1.
+     * @post - the player's score is updated in the ui.
+     */
+    public void point() {
+        // TODO implement
+
+        int ignored = table.countCards(); // this part is just for demonstration in the unit tests
+        env.ui.setScore(id, ++score);
+    }
+
+    /**
+     * Penalize a player and perform other related actions.
+     */
+    public void penalty() {
+        // TODO implement
+    }
+
+    public int score() {
+        return score;
+    }
+}
